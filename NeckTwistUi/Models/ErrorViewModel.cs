7c8554622553c6aa6aa7687a7d256cb3d5d74344
Jonathan Stendahl <jonathan.stendahl@live.se>
2018-04-07 20:55:04 +0200

    adding project

---
+++ NeckTwistUi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NeckTwistUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
